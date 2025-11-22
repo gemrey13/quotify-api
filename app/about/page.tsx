c633b37e077c9c8f24da632e928a80dda9850500
gemrey13 <gemreyranola@gmail.com>
2025-11-22 11:32:58 +0800

    add about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -13,7 +13,7 @@ function About() {
       <div className="relative bg-linear-to-br from-indigo-600 via-purple-600 to-pink-500 text-white py-20 overflow-hidden ">
         <div className="absolute inset-0 bg-black/10"></div>
         <div className="absolute inset-0 bg-[url('data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNjAiIGhlaWdodD0iNjAiIHZpZXdCb3g9IjAgMCA2MCA2MCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj48ZyBmaWxsPSJub25lIiBmaWxsLXJ1bGU9ImV2ZW5vZGQiPjxwYXRoIGQ9Ik0zNiAxOGMzLjMxIDAgNiAyLjY5IDYgNnMtMi42OSA2LTYgNi02LTIuNjktNi02IDIuNjktNiA2LTZ6TTI0IDQyYzMuMzEgMCA2IDIuNjkgNiA2cy0yLjY5IDYtNiA2LTYtMi42OS02LTYgMi42OS02IDYtNnoiIHN0cm9rZT0iI2ZmZiIgc3Ryb2tlLXdpZHRoPSIuNSIgb3BhY2l0eT0iLjEiLz48L2c+PC9zdmc+')] opacity-20"></div>
-        <div className="container mx-auto  relative max-w-5xl">
+        <div className="container mx-auto px-4 relative max-w-5xl">
           <h1 className="text-6xl mb-4 bg-clip-text text-transparent bg-linear-to-r from-white to-blue-100">
             About Quotify
           </h1>
--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const Navigation = () => {
   return (
     <nav className="border-b bg-white/80 backdrop-blur-sm sticky top-0 z-50">
-      <div className="container mx-auto px-4 py-4 flex items-center justify-between">
+      <div className="container mx-auto px-4 py-4 max-w-5xl flex items-center justify-between">
         <Link href="/" className="flex items-center gap-2">
           <Sparkles className="w-6 h-6 text-blue-600" />
           <span className="text-xl">Quotify</span>
